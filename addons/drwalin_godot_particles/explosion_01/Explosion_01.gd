286a89e3dc2cc9faacd3770ed5202c0d1628d439
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-03-18 16:29:46 +0100

    Added sparks.

--- addons/drwalin_godot_particles/explosion_01/Explosion_01.gd
+++ addons/drwalin_godot_particles/explosion_01/Explosion_01.gd
@@ -4,27 +4,35 @@ class_name Explosion_01;
 
 var emittingF:bool = false;
 var emittingS:bool = false;
+var emittingSp:bool = false;
 
 func Emit():
 	$Fire_01.emitting = true;
 	$Smoke.emitting = true;
+	$Sparks.emitting = true;
 	emittingF = true;
 	emittingS = true;
+	emittingSp = true;
 	$Fire_01.restart();
 	$Smoke.restart();
+	$Sparks.restart();
 
 func IsEmiting() -> bool:
-	return emittingF || emittingS || $Fire_01.emitting || $Smoke.emitting;
+	return emittingF || emittingS || $Fire_01.emitting || $Smoke.emitting || emittingSp || $Sparks.emitting;
 
 func _SmokeDone():
 	emittingS = false;
 	
 func _FireDone():
 	emittingF = false;
+	
+func _SparksDone():
+	emittingSp = false;
 
 func _ready():
 	$Smoke.finished.connect(_SmokeDone);
 	$Fire_01.finished.connect(_FireDone);
+	$Sparks.finished.connect(_SparksDone);
 	Emit();
 
 
